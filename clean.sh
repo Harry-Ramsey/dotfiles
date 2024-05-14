dc9ad49088a9e0abb30a240f5f168c7e431e2903
Harry Ramsey <harryramseybusiness@gmail.com>
2024-05-14 01:00:04 +0100

    Add lazy.nvim cache of plugins to clean script
    This commit adds the clean script so that cleaning up dotfiles also
    removes cached neovim plugins.

--- clean.sh
+++ clean.sh
@@ -3,3 +3,10 @@ for directory in */; do
 	echo "Removing $directory"
 	stow -D --target=$HOME $directory
 done
+
+cache_path=("~/.cache/nvim/" "~/.local/share/nvim/")
+
+for directory in ${cache_path[@]}; do
+	echo "Removing $directory"
+	rm -rf $directory
+done
