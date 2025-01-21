a24240cb127c8115a9242a88ad0d97c738e5bcce
Harry Ramsey <harryramseybusiness@gmail.com>
2025-01-21 10:22:00 +0000

    nvim: Enhance tabs and whitespace
    This commit enables whitespace to be shown as a dot and tabs to be shown
    as an arrow. This commit also enables all tabs to be converted to spaces.

--- nvim/.config/nvim/lua/ramsey/core/options.lua
+++ nvim/.config/nvim/lua/ramsey/core/options.lua
@@ -1,4 +1,11 @@
 vim.g.mapleader = " "
+
+vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
+
+vim.opt.list = true
+vim.opt.listchars = { tab = '→ ', space = '·' }
+
+vim.opt.clipboard:append('unnamedplus')
