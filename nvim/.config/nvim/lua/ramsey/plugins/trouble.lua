fe6cefb3ad9ced7db4e337647b7110a462cf4d9d
Harry Ramsey <harry.ramsey@arm.com>
2025-05-07 10:03:40 +0100

    NVIM: Enable LSP diagnostic information via trouble
    This commit adds a keybind to enable LSP diagnostic information for each
    buffer or for the whole of the project.

--- nvim/.config/nvim/lua/ramsey/plugins/trouble.lua
+++ nvim/.config/nvim/lua/ramsey/plugins/trouble.lua
@@ -5,9 +5,14 @@ return {
   keys = {
     {
       "<leader>tt",
-      function() require("trouble").toggle({ mode = "document_diagnostics" }) end,
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)"
+    },
+    {
+      "<leader>tT",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
       desc = "Buffer Diagnostics (Trouble)"
-    }
+    },
   },
 }
 
